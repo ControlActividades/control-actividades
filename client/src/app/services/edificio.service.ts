6236026aa6695a59060c8e1a5c7c438e011976e5
ZahirAndres <113659010+ZahirAndres@users.noreply.github.com>
2024-07-09 15:42:12 -0600

    Service-Models

--- client/src/app/services/edificio.service.ts
+++ client/src/app/services/edificio.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';//peticiones de cliente
 
 @Injectable({
   providedIn: 'root'
 })
 export class EdificioService {
 
-  constructor() { }
+  //API_URI = 'http://localhost:3000/api'; //backend 
+  API_URI = 'http://localhost:3000/edificio'; //frondend 
+
+  constructor(private http:HttpClient) { }
+  
+  getEdificios(){
+    return this.http.get(`${this.API_URI}`);
+  }
+
+  getEdificio(idEdificio:string){
+    return this.http.get(`${this.API_URI}/${idEdificio}`);
+  }
+  
 }
