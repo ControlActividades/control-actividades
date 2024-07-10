a18a1ad76979fe1fe022f7bea5665ca1bb348c3a
LuisFe5 <campuzanoluis568@gmail.com>
2024-07-10 14:34:08 -0700

    Subir actualizacion

--- client/src/app/services/oficio.service.ts
+++ client/src/app/services/oficio.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OficioService {
 
-  constructor() { }
+  API_URI = 'http://localhost:3000/oficio';
+
+  constructor(private http:HttpClient) { }
+
+  getOficios(){
+    return this.http.get(`${this.API_URI}`);
+  }
+
+  getOficio(idOficio:string){
+    return this.http.get(`${this.API_URI}/${idOficio}`);
+  }
 }
